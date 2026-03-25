203c2f892cd7bff8b6b76e003b82d46fa90c7746
yuvaraja2023-boop <yuvaraj.a2023@vitstudent.ac.in>
2026-03-25 19:07:56 +0530

    Update appTest.java

--- src/test/java/com/example/appTest.java
+++ src/test/java/com/example/appTest.java
@@ -1,1 +1,14 @@
+package com.example;
 
+import org.junit.test;
+import static org.junit.Assert.*;
+
+public class appTest {
+  
+  @Test
+  public void test.Add(){
+    App.app=new App();
+    assertEquals(5,app.add(2,3));
+  }
+}
+    
